b2c998cee4e3e79eacf82464b1f28551f47ac371
Roxaniii <92252445+Roxaniii@users.noreply.github.com>
2022-04-11 10:22:33 +0200

    Remove non-necessary files
    Created react app, removed non-necessary files and created a footer

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      This page was coded by{" "}
+      <a
+        href="https://github.com/Roxaniii?tab=repositories"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Roxane Kulenkamp
+      </a>
+    </p>
+  );
+}
