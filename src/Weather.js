ec73d9380c8fef882fc05821ae8fda0eb7a07eff
Roxaniii <92252445+Roxaniii@users.noreply.github.com>
2022-04-11 18:12:53 +0200

    Add styling (CSS)

--- src/Weather.js
+++ src/Weather.js
@@ -1,27 +1,37 @@
 import React from "react";
+import "./Weather.css";
+import "./App.css";
 
 export default function Weather() {
   return (
-    <div>
+    <div className="Weather">
       <form>
-        <input
-          type="search"
-          placeholder="Enter a city name"
-          className="form-control"
-        />
-        <input type="submit" value="search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city name"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
       </form>
       <h1>Berlin</h1>
       <ul>
         <li>Monday, 11:00</li>
         <li>Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-2 mb-5">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="weather icon"
           />
+          <span className="temperature">5</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
